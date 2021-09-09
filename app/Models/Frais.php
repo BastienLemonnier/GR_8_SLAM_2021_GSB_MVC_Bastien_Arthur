ad0ecc0481cfe55786fee0143498c3fd59bee568
BastienLemonnier <lemonnierbastien4@gmail.com>
2021-09-09 16:41:43 +0200

    ajout modeles principaux

---
+++ app/Models/Frais.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+class Frais extends Model
+{
+
+    public function getFraisMoisChoisis ()
+    {
+        $db = db_connect("guest");
+        
+        $sql = "SELECT quantite FROM LigneFraisForfait INNER JOIN FraisForfait ON LigneFraisForfait.idFraisForfait = FraisForfait.id
+        WHERE idVisiteur = ? AND mois LIKE ? ORDER BY FraisForfait.id;";
+        $resultat = $db -> query($id, $moisChoisis);
+        $resultat = $resultat -> getResult();
+
+        if(empty($resultat))
+        {
+            for($i = 0; $i < 4; $i ++)
+            {
+                $resultat[$i] = 0;
+            }
+        }
+
+        return $resultat;
+    }
+
+}
